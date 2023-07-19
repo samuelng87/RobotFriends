e2206e9f9ba70d99c0f2edcb352ad4760f6897d3
sam <samuel6249ng@gmail.com>
2023-07-19 13:01:27 +1000

    scroll

---
+++ src/Scroll.js
@@ -0,0 +1,15 @@
+import { over } from 'lodash';
+import React from 'react';
+
+
+const Scroll = (props) => {
+    return (
+        <div style={{overflowY:'scroll', border:'5px solid black', height: '500px'}}>
+            {props.children}
+        </div>
+    )
+    console.log('test')
+
+}
+
+export default Scroll;
